511042e0f4a5b8adf94987fdcbae57a0c9991527
Akshaya <ar0210@srmist.edu.in>
2026-02-23 19:29:54 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp - Use Case 1: Print OOPS to console.
+ * Foundational class for banner printing application.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    /**
+     * Main entry point for the application.
+     * Prints "OOPS" to the console.
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
